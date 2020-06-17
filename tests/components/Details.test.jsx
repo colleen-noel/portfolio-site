747b8e8c8c3327c65bfcbe52a733420a6266f054
Colleen Noel <noel.colleen@gmail.com>
2020-06-17 18:43:14 -0400

    complete2

--- tests/components/Details.test.jsx
+++ tests/components/Details.test.jsx
@@ -5,12 +5,20 @@ import { shallow } from 'enzyme'
 import Details from '../../components/Details'
 
 describe('Components - Details', () => {
-  it.only('displays the name, description, gitHub link, applications used, and picture of the selected project', () => {
+  it('displays the name, description, gitHub link, applications used, and picture of the selected project', () => {
     const project = {
-      name: 'Test', description: 'my project', gitHibLink: 'link', apps: '["some apps"]', pictureName: 'img src',
+      name: 'Test', description: 'my project', gitHubLink: 'link', apps: '["some apps"]', pictureName: 'image',
     }
     const wrapper = shallow(<Details project={project} />)
 
-    expect(wrapper.text()).to.equal('Test my project link some apps img src')
+    const div = wrapper.find('div')
+    const ul = wrapper.find('ul')
+
+
+    expect(wrapper.find('h2').text()).to.equal('Test')
+    expect(div.at(0).text()).to.equal('my project')
+    expect(div.at(1).text()).to.equal('link')
+    expect(ul.find('li').text()).to.equal('some apps')
+    expect(div.at(2).find('img').prop('src')).to.equal('image')
   })
 })
